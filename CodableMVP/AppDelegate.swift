ea2a8540c80ca66b531320841f98a38c64ac86f0
OuSS <ouss.cs@gmail.com>
2019-05-10 13:46:53 +0000

    Project done

--- CodableMVP/AppDelegate.swift
+++ CodableMVP/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        let animeListController = AnimesController()
+        let navController = UINavigationController(rootViewController: animeListController)
+        window?.rootViewController = navController
+        
         return true
     }
 
